53a508afe7e54e8269e376de2058f99a78534128
1106-Spencer-Pietrina <spencerpietrina@gmail.com>
2024-05-05 08:33:30 +0000

    insta

--- instagram.c
+++ instagram.c
@@ -3,13 +3,14 @@
 	//Date: 5/4/2024
 	//Final Project
 	
+
 #include <stdio.h>
 
-//#define MAX_ROW[100]
-//#define MAX_COL[100]
+#define MAX_ROW 100
+#define MAX_COL 100
 
 void mainMenu ();
-int loadImage();
+void loadImage();
 void editMenu();
 void saveFile();
 void readFile();
@@ -25,11 +26,12 @@ int main (){
 //Variables 
 int userNum = 0;
 int fileLoad = 0;
-int numRow, numCol;
+int numRow = 0;
+int numCol = 0;
 int picArray[100][100];
 char fileName[30];
 
-FILE *picFile;
+FILE *picFile, fp;
 
 mainMenu();
 
@@ -37,20 +39,19 @@ scanf("%d", &userNum);
 
 switch(userNum)
 {
+//Load File
 	case 1:
-		//functionload
-		
 		printf("What is the name of the image file?: ");
 		scanf("%s", fileName);
-		readFile(fileName);
+		//readFile(fileName);
 		
-		readFile(fileName, picArray, &numRow, &numCol);
+		//readFile(fileName, picArray, &numRow, &numCol);
 		
 		//Read file name from term?
 		
 		break;
+//Display Image
 	case 2:
-		//Displayimage
 		if(fileLoad == 0){
 		printf("Sorry, no image to display");
 		}
@@ -58,6 +59,7 @@ switch(userNum)
 		//displayimage function
 		}
 		break;
+//Edit
 	case 3:
 		//
 		if(fileLoad == 0){
@@ -67,6 +69,7 @@ switch(userNum)
 			editMenu();
 		}
 		break;
+//Exit
 	case 0:
 		printf("Goodbye!");
 		
@@ -103,23 +106,47 @@ void editMenu(){
 }
 
 	//READ FILE
-void readFile(const int *fileName, int picArray[100][100], int *num_col){
-	FILE *file = fopen(fileName, "r");
-	if(file == NULL){
+	//Having issues with this function, line 111 declarations 
+	
+void readFile(fileName, int numCol, int numRow, int picArray[][]){
+	
+	FILE *fp;
+	fp = fopen(*fileName, "r");
+	
+	if(fp == NULL){
 		printf("Error opening file!\n");
 		return;
 	}
 	
-	fscanf(file, "%d%d", numRow, numCol); 
+	fscanf(fp, "%d %d", numRow, numCol); 
 	
-	for(int i = 0; i < *num_col; i++){
-		for(int j = 0; j < *num_col){
-			fscanf(file, "%d", &picArray[i][j]);
+	for(int i = 0; i < *numCol; i++){
+		for(int j = 0; j < *numCol){
+			fscanf(fp, "%d", &picArray[i][j]);
 			}
 		}
-	fclose(fileName);
+	fclose(fp);
 }
 
+
+	//SAVE FILE
+	
+void saveFile(const char *fileName, int array[MAX_ROW][MAX_COL], int numRow, int numCol){
+	FILE *Savefp = fopen(fileName, "w");
+	if(Savefp == NULL){
+		printf("Error opening file!\n");
+		
+		return;
+	}
+	fprintf(Savefp, "%d %d", numRow, numCol);
+	
+	for(int i = 0; i < numRow; i++){
+		for(int j = 0; j < numCol; j++){
+			fprintf(Savefp, "%d", array[i][j]);
+		}
+		fprintf(Savefp, "\n");
+	}
+}
 
 
 
