3d97947b4d9ad906653e92eb123f622e677fc7b8
1106-Forest-Joe <joseph.forest@nevada.unr.edu>
2024-05-05 01:46:44 +0000

    1st commit

---
+++ instagram.c
@@ -0,0 +1,38 @@
+
+	//Author: Joe Forest, Pietrina Spencer
+	//Date: 5/4/2024
+	//Final Project
+	
+#include <stdio.h>
+
+void mainMenu ();
+
+
+int main (){
+
+//Variables 
+int userNum = 0;
+
+mainMenu ();
+scanf("%d", userNum);
+
+
+
+
+
+
+
+
+return 0;
+}
+
+//First Menu
+void mainMenu (){
+printf("**ERINSTAGRAM**\n");
+printf("1: Load image\n");
+printf("2: Display image\n");
+printf("3: Edit image\n");
+printf("0: Exit\n");
+printf("\nChoose from one of the options above: ");
+}
+
